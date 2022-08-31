bd96a8c4a290e0215c1cf94bce9020bddd6cf22e
Chris Jansen <janstenpickle@users.noreply.github.com>
2022-08-31 10:31:17 +0100

    Cross-build arm64 images
    This sets up the build to cross-build the JRE base images for both x86_64 and
    arm64.
    
    - Add manual step, with instructions, for building glibc binaries
    - Modify docker files to download the correct binaries for their architecture

--- docker/build.sh
+++ docker/build.sh
@@ -14,6 +14,7 @@ docker tag $ALPINE_IMAGE alpine-base
 # Using the --cache-from argument was tried, but due to the nature of the docker build itself
 # this failed. So instead we check to see if the Alpine base image SHA has changed
 
+
 BUILD_BASE=true
 BASE_SHA=$(docker images $ALPINE_IMAGE --no-trunc --format "{{.ID}}")
 BASE_SHA_FILE=/tmp/docker-builds/alpine-sha
@@ -26,25 +27,28 @@ if [[ -f $BASE_SHA_FILE ]]; then
 fi
 
 
-export BASE_IMAGE_NAME="permutive-liberica-base"
-export IMAGE_NAME="permutive-jre-liberica"
+export BASE_IMAGE_NAME="$REMOTE_REPO/permutive-liberica-base"
+export IMAGE_NAME="$REMOTE_REPO/permutive-jre-liberica-experimental"
+if [[ $PUSH == "push" ]]; then
+  export PUSHARG="--push"
+else
+  export PUSHARG=""
+fi
 
 
-function tag {
-  docker tag $1 $2
-  docker tag $1 $REMOTE_REPO/$2
-  if [ "$PUSH" == "push" ]; then
-    docker push $REMOTE_REPO/$2
-  fi
-}
+if !(docker buildx ls | grep liberica); then
+  docker buildx create --name liberica
+fi
+docker buildx use liberica
+docker buildx inspect --bootstrap
+
 
 pushd $(dirname "$0")
 
 if [[ "$BUILD_BASE" == "true" ]]; then
   pushd liberica-base
 
-  docker build . -t $BASE_IMAGE_NAME
-  tag $BASE_IMAGE_NAME $BASE_IMAGE_NAME
+  docker buildx build --platform linux/amd64,linux/arm64 -t $BASE_IMAGE_NAME $PUSHARG .
 
   echo $BASE_SHA > $BASE_SHA_FILE
   popd
@@ -55,11 +59,10 @@ pushd liberica-jre
 function build_jre {
     JRE=$1
     BUILD_BASE=$2
-    echo $3 | while read -r downloadUrl version sha1 latest latestInFeatureVersion; do
+    echo $3 | while read -r armDownloadUrl armSha1 x86DownloadUrl x86Sha1 version latest latestInFeatureVersion; do
       echo $version
       # Build an image for each JRE using the base image that includes glibc
 
-      TEMP_TAG=$sha1
       SANITISED_VERSION_BUILD=$(echo $version | sed -e 's/\+/b/')
       SANITISED_VERSION=$(echo $version | cut -d '+' -f 1)
 
@@ -69,7 +72,7 @@ function build_jre {
       BUILD_IMAGE=true
       CACHED_SHA_FILE=/tmp/docker-builds/jre-$SANITISED_VERSION_BUILD-sha
       if [[ -f $CACHED_SHA_FILE ]]; then
-          if [[ $(cat $CACHED_SHA_FILE | xargs) == $sha1 && "$BUILD_BASE" == "false" ]]; then
+          if [[ $(cat $CACHED_SHA_FILE | xargs) == "$armSha1$x86Sha1" && "$BUILD_BASE" == "false" ]]; then
             echo "JRE sha matches cache, not building $version"
             BUILD_IMAGE=false
           fi
@@ -77,30 +80,28 @@ function build_jre {
 
 
       if [[ "$BUILD_IMAGE" == "true" ]];then
-        docker build . --build-arg LIBERICA_PKG_URL=$downloadUrl --build-arg LIBERICA_VERSION=$version --build-arg LIBERICA_SHA1=$sha1 -t "$IMAGE_NAME:$TEMP_TAG"
-        tag "$IMAGE_NAME:$TEMP_TAG" "$IMAGE_NAME:$SANITISED_VERSION_BUILD"
-        tag "$IMAGE_NAME:$TEMP_TAG" "$IMAGE_NAME:$SANITISED_VERSION"
+        TAGS="-t $IMAGE_NAME:$SANITISED_VERSION_BUILD -t $IMAGE_NAME:$SANITISED_VERSION"
         if [ "$latest" == "true" ]; then
-          tag "$IMAGE_NAME:$TEMP_TAG" "$IMAGE_NAME:latest"
+          TAGS="$TAGS -t $IMAGE_NAME:latest"
         fi
         if [ "$latestInFeatureVersion" == "true" ]; then
-          tag "$IMAGE_NAME:$TEMP_TAG" "$IMAGE_NAME:$JRE"
+          TAGS="$TAGS -t $IMAGE_NAME:$JRE"
         fi
+        echo $TAGS
 
-        docker rmi $IMAGE_NAME:$TEMP_TAG
+        docker buildx build --platform linux/amd64,linux/arm64 --build-arg LIBERICA_PKG_URL_ARM=$armDownloadUrl --build-arg LIBERICA_PKG_URL_X86=$x86DownloadUrl --build-arg LIBERICA_SHA1_ARM=$armSha1 --build-arg LIBERICA_SHA1_X86=$x86Sha1 $TAGS $PUSHARG .
 
-        echo $sha1 > $CACHED_SHA_FILE
+        echo "$armSha1$x86Sha1" > $CACHED_SHA_FILE
       fi
     done
 }
 
 export SHELL=$(type -p bash)
 export -f build_jre
-export -f tag
 
 for JRE in $JRES; do
   # Use the Liberica API to get all of the releases for the major version
-  curl "https://api.bell-sw.com/v1/liberica/releases?version-feature=$JRE&bitness=64&os=linux&arch=x86&package-type=tar.gz&bundle-type=jre" 2> /dev/null | jq -r '.[] | [.downloadUrl, .version, .sha1, .latest, .latestInFeatureVersion] | @tsv' | parallel -j 4 "build_jre $JRE $BUILD_BASE {}"
+  curl "https://api.bell-sw.com/v1/liberica/releases?version-feature=$JRE&bitness=64&os=linux&arch=x86&arch=arm&package-type=tar.gz&bundle-type=jre" 2> /dev/null | jq -r 'group_by(.version)[] | [sort_by(.architecture)[]] | [.[0].downloadUrl, .[0].sha1, .[1].downloadUrl, .[1].sha1, .[0].version, .[0].latest, .[0].latestInFeatureVersion] | @tsv' | parallel -j 4 "build_jre $JRE $BUILD_BASE {}"
 done
 
 popd
